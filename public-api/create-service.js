ba2c752a2b0bbdcf9f44614febe5fce1a2a8e529
Zartosht Sepideman <zartosht@configure8.io>
2023-05-31 19:21:09 +0300

    C8-2716 Remove user agent

--- public-api/create-service.js
+++ public-api/create-service.js
@@ -65,7 +65,7 @@ function createServiceExample(apiKey) {
     headers: {
       "api-key": apiKey,
       "Content-Type": "application/json",
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+      "User-Agent": "Configure8",
       "Content-Length": data.length
     }
   };
